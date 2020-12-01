2041d7df99edf63f9e6ce6acf7452292b625e66c
Abhradeep-debug <cgargi2007@gmail.com>
2020-12-01 12:04:45 +0530

    updated Pig.js Slingshot.js and sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
+    box1 = new Box(700,320,70,70); ////
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
@@ -74,4 +74,13 @@ function mouseDragged(){
 
 function mouseReleased(){
     slingshot.fly();
+}
+
+function keyPressed(){
+if (keyCode === 32)
+{
+slingshot.attach(bird.body);
+
+}
+    
 }
